4fbb7dcf4f442332250395dd60584d4ad3ab96b7
Humma Noshine <Hummanoshine@gmail.com>
2022-03-22 16:25:48 +0000

    updating models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,37 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+// added below hn
+Products.belongsToMany(Category)
+
 // Categories have many Products
 
+// WHATS SOMETHING
+Category.something(Product, {
+
+})
 // Products belongToMany Tags (through ProductTag)
 
+//should i put unique in?product_id key??
+Product.belongsToMany(Tag, {
+  through: {
+  model: ProductTag,
+  },
+}),
+
 // Tags belongToMany Products (through ProductTag)
+// added below what to put inside???????????????is the through right?
+Tag.belongsToMany(Product, {
+  through: {
+    model:ProductTag
+  },
+}),
+
+
+
+
+
+
 
 module.exports = {
   Product,
